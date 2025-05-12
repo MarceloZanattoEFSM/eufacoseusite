18965da89f3b91f934713e979c2169479576e6b6
MarceloZanattoEFSM <marcelo.zanatto@eufacoseu.marketing>
2025-05-12 18:29:31 -0300

    vai

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   return (
     <header className={cn("fixed top-0 left-0 right-0 z-50 py-4 px-4 md:px-8 transition-all duration-300", scrolled ? "bg-white shadow-md" : "bg-transparent")}>
       <div className="max-w-7xl mx-auto flex justify-between items-center">
-        <Link to="/" className="flex items-center">
+        <Link to="#" className="flex items-center">
           <img alt="eufacoseu.site" className="h-10 md:h-12" src="/lovable-uploads/b2f5f879-9cd6-40b8-8e98-6190d4377290.jpg" />
         </Link>
         <Link to="#briefing" className="btn-primary text-sm md:text-base">
